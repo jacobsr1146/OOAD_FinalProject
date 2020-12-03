e5544847b1f7d75f723cc26f229e700063874ea7
SheldonRay <47021227+SheldonRay@users.noreply.github.com>
2020-12-02 18:38:46 -0700

    Added the Draft Pick variable

--- PlayerFactory.py
+++ PlayerFactory.py
@@ -8,12 +8,12 @@ class createPlayerFactory:  # Input will be a list of data where type[0] is the 
     @staticmethod
     def createPlayer(type):
         if type[4] == "G":
-            return Goalie(type[0], type[1], type[2], type[3], type[4], type[5], type[6], type[7], type[8], type[9], type[10], type[11])
+            return Goalie(type[0] ,type[1], type[2], type[3], type[4], type[5], type[6], type[7], type[8], type[9], type[10], type[11], type[12])
         if type[4] == "C":
-            return Center(type[0], type[1], type[2], type[3], type[4], type[5], type[6], type[7], type[8], type[9], type[10], type[11])
+            return Center(type[0],type[1], type[2], type[3], type[4], type[5], type[6], type[7], type[8], type[9], type[10], type[11], type[12])
         if type[4] == "RW":
-            return RightWinger(type[0], type[1], type[2], type[3], type[4], type[5], type[6], type[7], type[8], type[9], type[10], type[11])
+            return RightWinger(type[0],type[1], type[2], type[3], type[4], type[5], type[6], type[7], type[8], type[9], type[10], type[11], type[12])
         if type[4] == "LW":
-            return LeftWinger(type[0], type[1], type[2], type[3], type[4], type[5], type[6], type[7], type[8], type[9], type[10], type[11])
+            return LeftWinger(type[0],type[1], type[2], type[3], type[4], type[5], type[6], type[7], type[8], type[9], type[10], type[11], type[12])
         if type[4] == "D":
-            return Defenseman(type[0], type[1], type[2], type[3], type[4], type[5], type[6], type[7], type[8], type[9], type[10], type[11])
+            return Defenseman(type[0],type[1], type[2], type[3], type[4], type[5], type[6], type[7], type[8], type[9], type[10], type[11], type[12])
